3c64c6ede829c9215c9bc0060686973ebb38661c
Juan Arismendi <juanarismendidiaz@gmail.com>
2023-11-13 20:47:35 -0300

    models y db creados, falta troubleshoot(no se crean las tablas)

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
+const db = require("./models/db");
 
 const app = express();
 app.use(logger("dev"));
@@ -15,5 +16,11 @@ app.use(
   })
 );
 
-app.listen(5000, () => console.log(`Servidor  en el puerto 5000`));
+db.sync({ force: true })
+  .then(() => {
+    app.listen(5000, () => console.log(`Servidor  en el puerto 5000`));
+  })
+  .catch((error) => {
+    console.error("Error sincronizando la database:", error);
+  });
 module.exports = app;
