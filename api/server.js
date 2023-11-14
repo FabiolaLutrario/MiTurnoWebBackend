97c706581a69b1af7aaa2dfe48c5844f412f3869
Juan Arismendi <juanarismendidiaz@gmail.com>
2023-11-14 11:02:09 -0300

    fixed database bug

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 const db = require("./models/db");
+const { User, Turn, BranchOffice } = require("./models");
 
 const app = express();
 app.use(logger("dev"));
@@ -15,12 +16,11 @@ app.use(
     credentials: true,
   })
 );
+app.use((err, req, res, next) => {
+  res.status(500).send(err);
+});
 
-db.sync({ force: true })
-  .then(() => {
-    app.listen(5000, () => console.log(`Servidor  en el puerto 5000`));
-  })
-  .catch((error) => {
-    console.error("Error sincronizando la database:", error);
-  });
+db.sync({ force: false }).then(() => {
+  app.listen(5000, () => console.log(`Servidor  en el puerto 5000`));
+});
 module.exports = app;
