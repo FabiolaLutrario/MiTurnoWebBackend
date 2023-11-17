1fcb1fe5d3e55a0b726a00667e73fb25c482b55d
McHeph <141862378+McHeph@users.noreply.github.com>
2023-11-17 10:04:59 -0300

    quito console logs

--- api/server.js
+++ api/server.js
@@ -26,13 +26,6 @@ app.use((err, req, res, next) => {
 app.use("/api", routes);
 
 db.sync({ force: false }).then(() => {
-  // User.create({
-  //   fullName: "Admin",
-  //   dni: 1,
-  //   email: "juanarismendidiaz@gmail.com",
-  //   password: "Admin",
-  //   role: "Admin",
-  // });
   app.listen(5001, () => console.log(`Servidor  en el puerto 5000`));
 });
 module.exports = app;
