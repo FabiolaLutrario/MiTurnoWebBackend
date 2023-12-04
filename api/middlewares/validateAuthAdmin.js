52c3ab1f9907f36116d461fc76bd7d4ccd31590a
FabiolaLutrario <fabiolalutrario@gmail.com>
2023-12-04 14:01:13 -0300

    Modificaciones en condicionales de middlewares

--- api/middlewares/validateAuthAdmin.js
+++ api/middlewares/validateAuthAdmin.js
@@ -1,15 +1,16 @@
 const { validateToken } = require("../config/tokens.config");
 
 function validateAuthAdmin(req, res, next) {
-    const token = req.cookies.token;
-    if (!token) return res.sendStatus(401);
-  
-    const { user } = validateToken(token);
-    if (!user || user.role_id != "admin" || user.role_id != "super admin") return res.sendStatus(401);
-  
-    req.user = user;
-  
-    next();
-  }
-  
-  module.exports = { validateAuthAdmin };
+  const token = req.cookies.token;
+  if (!token) return res.sendStatus(401);
+
+  const { user } = validateToken(token);
+  if (!user || (user.role_id !== "admin" && user.role_id !== "super admin"))
+    return res.sendStatus(401);
+
+  req.user = user;
+
+  next();
+}
+
+module.exports = { validateAuthAdmin };
--- api/middlewares/validateAuthOperator.js
+++ api/middlewares/validateAuthOperator.js
@@ -1,15 +1,21 @@
 const { validateToken } = require("../config/tokens.config");
 
 function validateAuthOperator(req, res, next) {
-    const token = req.cookies.token;
-    if (!token) return res.sendStatus(401);
-  
-    const { user } = validateToken(token);
-    if (!user || user.role_id != "operator" || user.role_id != "admin" || user.role_id != "super admin") return res.sendStatus(401);
-  
-    req.user = user;
-  
-    next();
-  }
-  
-  module.exports = { validateAuthOperator };
+  const token = req.cookies.token;
+  if (!token) return res.sendStatus(401);
+
+  const { user } = validateToken(token);
+  if (
+    !user ||
+    (user.role_id !== "operator" &&
+      user.role_id !== "admin" &&
+      user.role_id !== "super admin")
+  )
+    return res.sendStatus(401);
+
+  req.user = user;
+
+  next();
+}
+
+module.exports = { validateAuthOperator };
