5f2d762534501b8bd680da7e3fa8d81c6383094c
Juan Arismendi <juanarismendidiaz@gmail.com>
2023-12-06 18:39:49 -0300

    created some fakedata

--- api/models/BranchOffice.models.js
+++ api/models/BranchOffice.models.js
@@ -34,4 +34,39 @@ BranchOffice.init(
   { sequelize: db, modelName: "branch_office" }
 );
 
+BranchOffice.sync()
+  .then(() => {
+    return BranchOffice.count();
+  })
+  .then((count) => {
+    if (count === 0) {
+      const branchOfficesToCreate = [
+        {
+          name: "Yerba Buena",
+          email: "juanarismendidiaz@gmail.com",
+          phone_number: "3814888082",
+          boxes: 4,
+          opening_time: "07:30",
+          closing_time: "21:30",
+        },
+        {
+          name: "San Miguel",
+          email: "juanarismendidiaz@gmail.com",
+          phone_number: "3814888082",
+          boxes: 4,
+          opening_time: "07:30",
+          closing_time: "21:30",
+        },
+      ];
+      return BranchOffice.bulkCreate(branchOfficesToCreate);
+    }
+    return Promise.resolve(); // No es necesario devolver nada si ya hay razones de cancelacióon
+  })
+  .then(() => {
+    console.log("Default branch offices created successfully.");
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
+
 module.exports = BranchOffice;
